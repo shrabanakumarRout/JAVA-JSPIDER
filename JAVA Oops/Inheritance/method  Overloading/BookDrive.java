495d75caa33938f001fd12dfa0b7adae9e3aab76
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-10-16 12:33:15 +0530

    ok

---
+++ JAVA Oops/Inheritance/method  Overloading/BookDrive.java
@@ -0,0 +1,15 @@
+public class BookDrive {
+public static void main(String [] args) {
+	Book b1 =BookDrive.createBook();
+	b1.display();
+	Book b2 = Bookdrive.createBook();
+	b2.display();
+
+
+}
+}
+
+
+
+
+
